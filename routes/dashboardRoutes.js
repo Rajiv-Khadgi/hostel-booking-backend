bb547bb4a51228244b6e2aadf9e72b2f4c8cf360
Rajiv Khadgi <rajivkhadgi444@gmail.com>
2026-02-11 09:36:03 +0545

    Route(dashboard): implemented routes for dashboard with authentication

---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { authenticate } from '../middleware/authMiddleware.js';
+import { getDashboardStats } from '../controllers/dashboardController.js';
+
+const router = express.Router();
+
+router.get('/', authenticate, getDashboardStats);
+
+export default router;
